c1aa077142943b30d92529df77bf91308fa388a3
Kári Tristan Helgason <kthelgason@gmail.com>
2014-10-13 11:45:07 +0000

    Refactored arg parsing into separate method

--- http_proxy.py
+++ http_proxy.py
@@ -1,8 +1,20 @@
+import sys
 import argparse
 
-if __name__ == '__main__':
+
+
+def main():
     parser = argparse.ArgumentParser(
             description="A command-line http proxy server.")
     parser.add_argument('port', type=int, help="The port to start the server on")
     parser.add_argument('logfile', help='The logfile to log connections to')
     args = parser.parse_args()
+
+    return 0
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        print "Goodbye!"
