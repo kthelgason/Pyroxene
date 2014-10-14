372bca5074d3ae96c46df8860412851427debdbd
JoiBlumen <joiblumen@gmail.com>
2014-10-14 01:24:37 +0000

    Split response and requests. Make sure to stop recv when done.

--- http_proxy.py
+++ http_proxy.py
@@ -3,36 +3,26 @@ from __future__ import print_function
 import sys
 import socket
 import argparse
+import os
 
 
 LISTEN_ADDRESS = ''
-BUFSIZE = 4096
+BUFSIZE = 8192
 SUPPORTED_METHODS = ['GET', 'POST', 'HEAD']
+SUPPORTED_VERSIONS = {'HTTP/1.1'}
 
 class ConnectionContext(object):
 
     def __init__(self, client):
         self.client_sock = client
-        self.parse_packet()
-
-    def parse_packet(self):
-        """
-        request-line = method SP request-target SP HTTP-version CRLF
-        *( header-field CRLF)
-        CRLF
-        [ message-body ]
-        """
+        self.request_headers = {}
+        self.response_headers = {}
         message = self.client_sock.recv(BUFSIZE)
-        lines = message.split('\r\n')
-        method, path, protocol_version = lines.pop(0).split()
-        if not method in SUPPORTED_METHODS:
-            self.send_unsupported_method_error()
-        headers, host = self.parse_headers(lines)
-        print(headers)
-        self.server_sock = self.connect_to_server(host)
-        self.server_sock.send(message)
-        self.proxy_loop()
-
+        if message:
+            self.parse_request(message)
+            self.server_sock = self.connect_to_server(self.request_headers.get('host'))
+            self.server_sock.send(message)
+            self.proxy_loop()
 
     def connect_to_server(self, host):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,29 +30,72 @@ class ConnectionContext(object):
         sock.connect((host_addr, 80))
         return sock
 
+    def split_message(self, message):
+        """
+         HTTP-message = start-line CRLF
+                        *( header-field CRLF )
+                        CRLF
+                        [ message-body ]CRLF
+        """
+        line, rest = message.split('\r\n', 1)
+        headers, data = rest.split('\r\n\r\n', 1)
+        return line, headers, data
 
-    def parse_headers(self, data):
-        host = ""
-        headers = []
-        while data[0] != '':
-            header_line = data.pop(0)
-            if header_line.startswith("Host:"):
-                host = header_line.split()[1]
-            headers.append(header_line)
+    def parse_request(self, message):
+        line, headers, data = self.split_message(message)
+        method, path, protocol_version = line.split()
+        if not method in SUPPORTED_METHODS:
+            self.send_unsupported_method_error()
+        self.request_headers = self.parse_headers(headers)
 
-        return headers, host
+    def parse_response(self, message):
+        if not message:
+            return
+        line, headers, data = self.split_message(message)
+        protocol_version, code, reason = line.split(' ', 2)
+        if not protocol_version in SUPPORTED_VERSIONS:
+            self.send_unsupported_version_error()
+        self.response_headers = self.parse_headers(headers)
+        #return len(line) + len(headers) + 6
+        return line, headers, data
+
+    def parse_headers(self, header_lines):
+        headers = {}
+        header_lines = header_lines.split('\r\n')
+        for header in header_lines:
+            parts = header.split(':')
+            key = parts[0].strip()
+            value = ':'.join(parts[1:]).strip()
+            headers[key.lower()] = value.lower()
+        return headers
 
     def send_unsupported_method_error(self):
         print("Unsupported Method!")
 
-    def proxy_loop(self):
-        while True:
-            data = self.server_sock.recv(BUFSIZE)
-            print(data)
-            if not data:
-                break
-            self.client_sock.send(data)
+    def send_unsupported_version_error(self):
+        print("Unsupported Protocol Version!")
 
+    def proxy_loop(self):
+        msg = self.server_sock.recv(BUFSIZE)
+        line, headers, data = self.parse_response(msg)
+        content_length = sys.maxint
+        if "content-length" in self.response_headers:
+            content_length = int(self.response_headers.get("content-length"))
+        while len(data) != content_length and not data.endswith('\r\n\r\n'):
+            msg = self.server_sock.recv(BUFSIZE)
+            if not msg:
+                break
+            data += msg
+        packet = line + '\r\n' + headers + '\r\n\r\n' + data
+        self.client_sock.send(packet)
+
+    def recv_all(self, s):
+        total_data=[]
+        while True:
+            data = s.recv(BUFSIZE)
+            if not data: break
+            total_data.append(data)
+        return ''.join(total_data)
 
 class ProxyServer(object):
 
